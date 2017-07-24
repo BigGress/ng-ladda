bec962abec45d93f1b5bf0477a0176079a4e1e01
gress <451148156@qq.com>
2017-07-24 21:36:47 +0800

    fix(基础): 优化开发环境

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { LaddaPage } from './app.po';
+
+describe('ladda App', () => {
+  let page: LaddaPage;
+
+  beforeEach(() => {
+    page = new LaddaPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
