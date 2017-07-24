967cfd10054fe7a75ad39a98ee2c43174f8b2f23
gress <451148156@qq.com>
2017-07-24 22:48:32 +0800

    feat(example): make a example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+
+  test(data: any) {
+    data.return = Observable.create((observer) => {
+      setTimeout(() => {
+        observer.next('test');
+      }, 500)
+    });
+  }
+
+  testPromise(data) {
+    data.return = new Promise((res) => {
+      setTimeout(() => {
+        res('next')
+      }, 500)
+    })
+  }
 }
